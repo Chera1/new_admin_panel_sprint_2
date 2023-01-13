e99f15b32fd7db0c50d88353d4b60364be08675c
iManapov <manapovilnar@gmail.com>
2023-01-13 12:36:18 +0300

    Some improvements

--- docker_compose/simple_project/app/example/settings.py
+++ docker_compose/simple_project/app/example/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://{host}}:{port}/0'.format(host=os.environ.get('REDIS_HOST', '127.0.0.1'),
+CELERY_BROKER_URL = 'redis://{host}:{port}/0'.format(host=os.environ.get('REDIS_HOST', '127.0.0.1'),
                                                       port=os.environ.get('REDIS_PORT', 6379))
 
 # CELERY_RESULT_BACKEND = "db+postgresql://app:123qwe@localhost:5432/movies_database"
@@ -153,7 +153,7 @@ CELERY_RESULT_BACKEND = "django-db"
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://{host}}:{port}/1'.format(host=os.environ.get('REDIS_HOST', '127.0.0.1'),
+        'LOCATION': 'redis://{host}:{port}/1'.format(host=os.environ.get('REDIS_HOST', '127.0.0.1'),
                                                       port=os.environ.get('REDIS_PORT', 6379)),
     }
 }
--- docker_compose/simple_project/app/templates/tasks.py
+++ docker_compose/simple_project/app/templates/tasks.py
@@ -8,19 +8,18 @@ from movies.models import FilmWork
 @shared_task
 def send_mail_massage(*args, **kwargs):
     # Тут будет отправка в API
-    request = post('url', json=kwargs)
+    request = post('http://localhost:8003/api/v1/notifications/mailing/all', json=kwargs)
     return 'Successful! ' + str(request.status_code)
 
 
 @shared_task
 def most_popular_films(*args, **kwargs):
     list_of_films = FilmWork.objects.filter(rating__gte=6)
-    ids_films = [{"id": each_film.id,
+    ids_films = [{"id": str(each_film.id),
                   "title": each_film.title,
                   "rating": each_film.rating} for each_film in list_of_films]
     kwargs['context'].update(movies=ids_films)
     # Тут будет отправка в API
-    request = post('url', json=kwargs)
+    request = post('http://localhost:8003/api/v1/notifications/mailing/all', json=kwargs)
     result = request.status_code
     return result
-
